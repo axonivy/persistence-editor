cb5ad23aa6df964f2d7c6d97664dc6a9df75ddc2
Lukas Lieb <lukas.lieb@axonivy.com>
2026-07-27 11:00:19 +0200

    Use BasicMultiCombobox

--- packages/editor/src/editor/sidebar/components/ManagedClassesCombobox.test.tsx
+++ packages/editor/src/editor/sidebar/components/ManagedClassesCombobox.test.tsx
@@ -37,42 +37,6 @@ const renderCombobox = (data?: Array<string>) => {
   return { data: () => value };
 };
 
-test('select', async () => {
-  const { data } = renderCombobox();
-  const input = screen.getByRole('combobox');
-  expect(input).toHaveAttribute('data-value', 'com.test.Employee');
-  expect(screen.queryByRole('listbox')).not.toBeInTheDocument();
-
-  await userEvent.click(input);
-  expect(screen.getByRole('listbox')).toBeVisible();
-  console.log(screen.getAllByRole('option').map(option => option.textContent));
-  expect(screen.getAllByRole('option')).toHaveLength(4);
-  expect(screen.getByRole('option', { name: 'com.test.Employee' })).toHaveAttribute('data-selected');
-  await userEvent.click(screen.getByRole('option', { name: 'com.test.Teamleader' }));
-  expect(data()).toEqual(['com.test.Employee', 'com.test.Teamleader']);
-});
-
-test('select can be handled with keyboard', async () => {
-  const { data } = renderCombobox();
-  const input = screen.getByRole('combobox');
-  expect(input).toHaveAttribute('data-value', 'com.test.Employee');
-  await userEvent.keyboard('[Tab]');
-  expect(input).toHaveFocus();
-  expect(screen.queryByRole('listbox')).not.toBeInTheDocument();
-
-  await userEvent.keyboard('[ArrowDown]');
-  expect(screen.getByRole('listbox')).toBeInTheDocument();
-  expect(screen.getAllByRole('option')).toHaveLength(4);
-  expect(screen.getByRole('option', { name: 'com.test.Employee' })).toHaveAttribute('data-selected');
-  expect(screen.getByRole('option', { name: 'com.test.Employee' })).toHaveAttribute('data-highlighted');
-  await userEvent.keyboard('h');
-  expect(screen.getAllByRole('option')).toHaveLength(1);
-  await userEvent.keyboard('[ArrowDown]');
-  expect(screen.getByRole('option', { name: 'com.test.HRManager' })).toHaveAttribute('data-highlighted');
-  await userEvent.keyboard('[Enter]');
-  expect(data()).toEqual(['com.test.Employee', 'com.test.HRManager']);
-});
-
 test('unknown value', async () => {
   const { data } = renderCombobox(['unknown']);
   const input = screen.getByRole('combobox');
@@ -82,11 +46,4 @@ test('unknown value', async () => {
   expect(screen.getAllByRole('option')).toHaveLength(5);
   expect(screen.getByRole('option', { name: 'unknown.unknown' })).toHaveAttribute('data-selected');
   expect(data()).toEqual(['unknown']);
-});
-
-test('readonly mode', () => {
-  customRender(<ManagedClassesCombobox value={['com.test.Employee']} onChange={() => {}} entityClasses={entityClasses} />, {
-    wrapperProps: { readonly: true }
-  });
-  expect(screen.getByRole('combobox')).toBeDisabled();
 });
