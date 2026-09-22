06dc5d313080b41a1c24df0dd8b2df569b560708
ivy-edp <elio.dipuma@axonivy.com>
2026-09-22 14:35:03 +0200

    XIVY-19449 Add data source selection when creating persistence units

--- playwright/tests/page-objects/AddPersistenceDialog.ts
+++ playwright/tests/page-objects/AddPersistenceDialog.ts
@@ -1,10 +1,12 @@
 import { type Locator, type Page } from '@playwright/test';
+import { Select } from './components/Select';
 import { Textbox } from './components/Textbox';
 
 export class AddPersistenceDialog {
   readonly page: Page;
   readonly locator: Locator;
   readonly name: Textbox;
+  readonly dataSource: Select;
   readonly cancel: Locator;
   readonly create: Locator;
 
@@ -12,6 +14,7 @@ export class AddPersistenceDialog {
     this.page = page;
     this.locator = this.page.getByRole('dialog');
     this.name = new Textbox(this.locator, { name: 'Name' });
+    this.dataSource = new Select(page, this.locator, { name: 'Data Source' });
     this.cancel = this.locator.getByRole('button', { name: 'Cancel' });
     this.create = this.locator.getByRole('button', { name: 'Create' });
   }
